06d4c9fbc2d170af504a9700cb6818e38895b05c
lioc3sar <juliocesaoguimaraes@gmail.com>
2021-09-24 00:34:52 -0300

    Aula 01 - Liftoff

---
+++ src/services/firebase.ts
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDOBZuhKAtMELJ_aJJLPj_GTYHJG-KtRBk",
+  authDomain: "letmeask-183d0.firebaseapp.com",
+  databaseURL: "https://letmeask-183d0-default-rtdb.firebaseio.com",
+  projectId: "letmeask-183d0",
+  storageBucket: "letmeask-183d0.appspot.com",
+  messagingSenderId: "946924112771",
+  appId: "1:946924112771:web:46ffee394928239302c4b6"
+};
+
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const database = firebase.database();
+
+
+
+
+
+
